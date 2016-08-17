8df48955f4450a279b2a0b40338afd4a337b20a4
Anderson <anderson.mosquera9610@gmail.com>
2016-08-16 21:28:31 -0500

    Terminando la multiplicacion entre un vector y una matriz

---
+++ Talleres/MultiMV.py
@@ -0,0 +1,35 @@
+def Matriz(lon):
+    d = [0]*lon
+    for i in range(lon):
+        d[i] = [0]*lon
+    for i in range(lon):
+        for j in range(lon):
+            d[i][j] = int(raw_input("Numero: "))
+
+    return d
+
+def Vector(lon):
+    d = [0]*lon
+    for i in range(lon):
+        d[i] = int(raw_input("Numero: "))
+
+    return d
+def Multiplicar(matriz, vector, lon):
+    res = [0]*lon
+    aux = 0;
+    for i in range(lon):
+        for j in range(lon):
+            res[i] += matriz[i][j] * vector[j]
+    print "Matriz Origen: "
+    for i in matriz:
+        print i
+    print "Vector Origen: "
+    for i in vector:
+        print "[%d]" % i
+    print "Resultado: "
+    for i in res:
+        print "[%d]" % i
+
+M = Matriz(3)
+V = Vector(3)
+Multiplicar(M,V,3)
